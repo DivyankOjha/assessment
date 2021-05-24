8edd60dcc5354cb1aacda116780659eb79bf5c59
Divyank Ojha <54564161+DivyankOjha@users.noreply.github.com>
2021-05-24 16:04:04 +0530

    tried L1

--- app.js
+++ app.js
@@ -1,44 +1,108 @@
-const converter = require('json-2-csv');
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const converter = require("json-2-csv");
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const testRouter = require('./routes/testRoute');
+const testRouter = require("./routes/testRoute");
 
 // const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
 
   next();
 });
 //  MIDDLEWARES
 
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: "50mb" }));
 
 app.use(
   bodyParser.urlencoded({
-    limit: '50mb',
+    limit: "50mb",
     extended: true,
     parameterLimit: 5000,
   })
 );
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // ROUTES
 
-app.use('/api/test', testRouter);
+app.use("/api/test", testRouter);
+app.use("/t", (req, res, next) => {
+  // var XLSX = require("xlsx");
+  // var workbook = XLSX.readFile("node.csv");
+  // var sheet_name_list = workbook.SheetNames;
+  // let arr = [];
+  // sheet_name_list.forEach(function (y) {
+  //   var worksheet = workbook.Sheets[y];
+  //   var headers = {};
+  //   var data = [];
+  //   for (z in worksheet) {
+  //     if (z[0] === "!") continue;
+  //     //parse out the column, row, and value
+  //     // var col = z.substring(0, 1);
+  //     // var row = parseInt(z.substring(1));
+  //     var col = z.replace(/[0-9]/g, "");
+  //     var row = parseInt(z.replace(/\D/g, ""));
+  //     var value = worksheet[z].v;
+
+  //     //store header names
+  //     if (row == 1) {
+  //       headers[col] = value;
+  //       continue;
+  //     }
+
+  //     if (!data[row]) data[row] = {};
+  //     data[row][headers[col]] = value;
+  //   }
+  //   //drop those first two rows which are empty
+  //   data.shift();
+  //   data.shift();
+  //   console.log(data);
+  //   for (i in data) {
+  //     if (data[i].Date) {
+  //       console.log("hi");
+  //       arr.push(data[i]);
+  //     }
+  //   }
+  // });
+  const reader = require("xlsx");
+  const file = reader.readFile("./node.xlsx");
+
+  let data = [];
+
+  const sheets = file.SheetNames;
+
+  for (let i = 0; i < sheets.length; i++) {
+    const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]]);
+    temp.forEach((res) => {
+      data.push(res);
+    });
+  }
+
+  for (i in data) {
+    if (data[i].Person == "A") {
+      console.log("got a");
+    }
+  }
+  
+
+  res.status(200).json({
+    status: "success",
+    data,
+  });
+});
 
 // app.use(globalErrorHandler);
 
--- routes/testRoute.js
+++ routes/testRoute.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const testController = require('../controllers/testController');
+const testController = require("../controllers/testController");
 
-router.post('/test-assessement-l2', testController.testAssessmentL2);
+router.post("/test-assessement-l1", testController.testAssessmentL1);
+router.post("/test-assessement-l2", testController.testAssessmentL2);
 
 module.exports = router;
